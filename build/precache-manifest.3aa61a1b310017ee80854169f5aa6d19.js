8c8b6124ad136f37d2400535d5aa19cf51765c4d
basarmemis <basar.memis@gmail.com>
2020-05-31 14:21:04 +0300

    First Trial

---
+++ build/precache-manifest.3aa61a1b310017ee80854169f5aa6d19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f04d8aa7f0e03aa162e5acc8995c803",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9ac22ff7a1d192a4e530",
+    "url": "/monsters-rolodex/static/css/main.adbad31e.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ac22ff7a1d192a4e530",
+    "url": "/monsters-rolodex/static/js/main.2076a17a.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
